fed3da0c96f9ff66d2fbe495735d9a4dc8b1ef31
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-05-30 14:36:32 -0400

    updated css added media queries

--- index.js
+++ index.js
@@ -208,6 +208,9 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artist[artist.name[0]]);
+console.log(artist[artist.bio[2]]);
+
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
