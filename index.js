3549dde705103e9faf8a925521dc1c6e000db712
Aaron Flowers <flowersa29@gmail.com>
2020-05-30 14:06:26 -0500

    sprint

--- index.js
+++ index.js
@@ -207,11 +207,12 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
-
+      a
+      console.log(artist[0]) "Amedeo Modigliani",
+      console.log(artist[2])"Diego Rivera",
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artist.splice(9, 1, Vincent Van Gogh )
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -223,9 +224,10 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(id, name) {
-    /* code here */
+    console.log(artist[0] 'is' name)
   }
-  
+  getArtistByIndex(0,1 )
+
   /**
 
 
