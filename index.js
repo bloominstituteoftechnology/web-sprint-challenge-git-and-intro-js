4b92de6ad6e8fb805ba7232113ca71da9a493234
Kevin Aparicio <apariciodkevin@gmail.com>
2020-08-01 13:45:52 -0500

    Failed Test

--- index.js
+++ index.js
@@ -208,9 +208,24 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0]);
+
+console.log(artists[2]);
+
+
+
+
+
+
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+
+
+artists[8].name = "Vincent Van Gogh";
+console.log(artists[8]);
+
+
 
 
 
@@ -223,9 +238,10 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
   
+}
+  
+  getArtistByIndex(artists, 4)
   /**
 
 
