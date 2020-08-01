06515ce533026ca2faffc6ff7b3cc73cf9229c2c
cameronyoung94 <youngcameron0794@gmail.com>
2020-07-31 22:08:16 -0400

    did questions 1 2 3 and 5

--- index.js
+++ index.js
@@ -207,11 +207,11 @@ const artists = [
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
-
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists[8].name = "Vincent Van Gogh";
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -223,9 +223,10 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
-  
+  if (index === 0)
+  return `The artist at index ${artists[0].id}, ${artists[0].name},`
+}
+  console.log(getArtistByIndex(artists, 0));
   /**
 
 
@@ -248,9 +249,14 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
+function removeArtist(array, artistindex) {
+  array.splice(artistindex, 1);
+  console.log(array.length);
+}
+
+removeArtist(artists, 0);
+
+console.log(artists)
   
  
 
