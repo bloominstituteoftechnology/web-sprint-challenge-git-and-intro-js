6a81e76ef7e5a0d7415e874627830cbf4244baa8
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-08-02 19:41:11 -0700

    task4

--- index.js
+++ index.js
@@ -236,12 +236,16 @@ function getArtistByIndex(array, index) {
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
-}
-
+function get20s(data){
+  const newArray = [];
+   if (data.includes(1900-2000)){
+     newArray.push(artists.name);
+   }
+   return newArray;
+ 
+ }
+ console.log(get20s(artists));
+ 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
