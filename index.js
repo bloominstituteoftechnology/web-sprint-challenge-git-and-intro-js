59bdcce84806b59cb7d4e275d2e2ae7194b77e18
Chris Elles <chriselles@alumni.gsb.stanford.edu>
2020-08-03 14:28:49 +1200

    Task 3

--- index.js
+++ index.js
@@ -226,12 +226,11 @@ console.log("The 9th artist's name is now correct:  ", artists[8].name);
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    const whichArtist= `The artist at ${index} is ${array[index].name}`;
+       return whichArtist;
   }
+  console.log(getArtistByIndex(artists, 0));
   
-  /**
-
-
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
 function get20s(/* Code here */){
