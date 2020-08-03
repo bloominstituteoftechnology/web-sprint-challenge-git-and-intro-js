cc4a8ace159c6171d8d5d152c02139e1c0580db9
Shuna Thomas <shuna.thomas@gmail.com>
2020-08-03 00:00:15 -0400

    submitting sprint challenge

--- index.js
+++ index.js
@@ -208,9 +208,17 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+1. function getArtist(artists, i) {
+  name = artists[i];
+  return name;
+}
+   getArtist(artist, 2);
+  console.log(name);
+
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+
 
 
 
