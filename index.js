fc527e3dddd3ee278d277376933daa77cff91a50
cameronyoung94 <youngcameron0794@gmail.com>
2020-08-03 23:42:18 -0400

    finished the sprintgit add .

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ console.log(artists[0].name);
 console.log(artists[2].bio);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-artists[8].name = "Vincent Van Gogh";
+console.log(artists[8].name = "Vincent Van Gogh");
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -231,12 +231,19 @@ function getArtistByIndex(array, artistindex) {
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
+function get20s(century){
+  let Array = [];
 
-function get20s(/* Code here */){
-
-  
-
+  for (let i = 0; i < century.length; i++){
+    let artistYears = century[i].years.split('-')
+    if (artistYears[0] >= 1900 && artistYears[1] <= 2000){
+       Array.push(century[i].name)
+       }
+  }
+  return Array
 }
+  console.log(get20s(artists));
+
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
