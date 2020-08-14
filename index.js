4b4ae434b63406dcc14adba8737048e23b89c1d5
Simone Ballard <simone.a.ballard@gmail.com>
2020-08-14 11:34:08 -0600

    JS all MVP answered

--- index.js
+++ index.js
@@ -208,11 +208,17 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
-/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+/* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */ /*He is acutally the 8th*/
 
-
+function fixName(array, index, name){
+  array[index].name = name;
+  return array;
+}
+/* console.log(fixName(artists, 8, Vincent Van Gogh)); */
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -223,19 +229,26 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return 'The artist at index ${array[index]} is ${array[index].name}.'
   }
+  console.log(getArtistByIndex(artists, 2));
   
   /**
 
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
+let twenties = []
 
-  /* Code here */
-
+function get20s(array){
+  for(let i = 0; i < array.length; i++){
+    if(array[i].years > 1900 && array[i].years < 2000){
+      twenties.push(artists[i].name);
+    }
+  }
+  return twenties;
 }
+console.log(get20s(artists));
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -248,8 +261,14 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array, index) {
+    for(let i = 0; i < array.length; i++){
+      if(array[i] === i){
+        array.splice(i);
+        console.log(array.length);
+      }
+    }
+    return array;
   }
   
  
@@ -267,11 +286,15 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/* Code here */){
-
-    /* Code here */
-
+function addArtist(id, name, years, genre, nationality, bio){
+newartists = [artists];
+  id = {name, years, genre, nationality, bio};
+  newartists.push({id});
+  return newartists;
   }
+
+  /*console.log(addArtist(id, Gina Pellon, 1926-2014, Abstract, Cuban, Gina Pellón (December 26, 1926 – March 27, 2014) was a Cuban painter who lived in France.[1] She was known for her abstract expressive paintings in strong colours, usually depicting women.[2] She also wrote collections of poems.));
+
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
@@ -281,11 +304,19 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
+let prolific = []
 
-  /* Code here */
+function lotsOfArt(artists){
+  for(let i = 0; i < artists.length; i++){
+    if(artists[i].paintings > 100){
+    prolific.push(artists[i].name);
+    }
+
+    return prolific;
 
 }
+
+console.log(lotsOfArt(artists));
 
 
 
