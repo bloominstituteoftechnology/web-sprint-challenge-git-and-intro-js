6f89202bef808f4a66008b2f7a7a163970eb601b
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-10-09 09:02:53 -0700

    task 2 javascript done

--- index.js
+++ index.js
@@ -212,9 +212,14 @@ Practice accessing data above by console.log-ing following items:
 console.log(artists[0].name);
 console.log(artists[2].bio);
 
-/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
+/* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 -- 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+function changeName(array, index, name){
+  array[index].name = name;
+  return array;
+}
+console.log(changeName(artists, 8, 'Vincent Van Gogh'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
