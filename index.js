fb4b6dc0f524ed80c0a5eb13f6b0d42bd086bbcb
Jessica Chu <chu.jessj@gmail.com>
2020-10-24 21:39:59 -0400

    did tast 1-4. Task 4 broke my npm

--- index.js
+++ index.js
@@ -209,11 +209,18 @@ Practice accessing data above by console.log-ing following items:
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+function changeArtistName (array, index, artistName) {
+  array[index].name = artistName;
+  return array;
+}
 
+console.log(changeArtistName(artists, 8, "Vincent Van Gogh"));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -225,18 +232,31 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artistsArray, index) {
+  const getArtistByIndexResult = artistsArray[index].name;
+  return `the artist at index ${index} is ${getArtistByIndexResult}`;
 }
+
+console.log(getArtistByIndex(artists, 0));
 
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(artistsArray) {
+  let newArtistsArray = [];
+  for (let i = 0; i < artistsArray.length; i++) {
+    const artist = newArtistsArray[i];
+    const artistBirth = parseInt(artist.years.substring(0, 4));
+    const artistDeath = parseInt(artist.years.substring(7));
+    if (artistBirth > 1900 && artistDeath < 2000) {
+      newArtistsArray.push(artistsArray[i].name);
+    } 
+  } 
+  return newArtistsArray;
 }
+console.log(get20s(artists));
 
 
 
