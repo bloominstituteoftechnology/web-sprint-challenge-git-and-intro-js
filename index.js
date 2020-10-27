7a696498683b9684c83217684663d319b93cba89
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2020-10-26 18:20:51 -0600

    I completed task 3

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ console.log(artists[2].bio);
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+console.log(artists[8].name = ('Vincent Van Gough'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called `getArtistByIndex` that takes two arguments:
@@ -227,10 +227,13 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
+function getArtistByIndex(array, index) {
   /*Your Code Here*/
-}
+  let myArtists = array[index];
+  return `The artist at index 0 is ${myArtists.name}` 
 
+}
+console.log(getArtistByIndex(artists, 0))
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
