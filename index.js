74f1ad9e5ba7e80bd898ef04d6ef06245d8bf69d
Brit Hemming <hello@brittanyhemming.com>
2020-11-06 12:18:32 -0500

    Update index.js
    updated 't' to lowercase in task 3 to match test

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * getArtistByIndex returns a string in the format `The artist at index {id} is {name}`
  * 
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
- * it will return `The artist at index 0 is Amedeo Modigliani`
+ * it will return `the artist at index 0 is Amedeo Modigliani`
 */
 function getArtistByIndex(/*Your Code Here*/) {
   /*Your Code Here*/
