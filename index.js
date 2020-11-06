31184c6e5896560e674a72a8a10a0bc6cfdc68ef
Mark <mark@Marks-MacBook-Air.local>
2020-11-06 12:18:47 -0500

    finished task 3

--- index.js
+++ index.js
@@ -209,11 +209,16 @@ Practice accessing data above by console.log-ing following items:
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+// console.log("the artist at index 0 is " + artists[0]["name"])
+// console.log(`the bio at index 2 is ${artists[2]["bio"]}`)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+// newName = "Vincent Van Gogh"
+// artists[8]["name"].push(newName)
+// console.log(artists[8]["name"])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -225,9 +230,14 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+
+function getArtistByIndex(array, index) {
+  let arr = array[index]["id"]
+  let name = array[index]["name"]
+  return `the artist at index ${arr} is ${name}`
 }  
+
+getArtistByIndex(artists, [0])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called get20s() 
