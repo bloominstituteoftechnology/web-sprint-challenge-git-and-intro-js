b9dbecafa12a9dfdbf4b5991612ca4a42997ef5c
Nathan Chagar <nathan.adel.chagar@gmail.com>
2020-11-06 10:47:55 -0800

    Missed mvp

--- index.js
+++ index.js
@@ -208,12 +208,20 @@ Practice accessing data above by console.log-ing following items:
 
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
-
+console.log(artists[0].name)
+console.log(artists[2].bio)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+function changeName(array,newName){
+  for(i in array){
+    if(array[i].name != newName){
+      array[i].name = newName
+    }
+  }
+}
+changeName(artists,"Vincent van Gogh")
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -225,9 +233,11 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artistsArray,num) {
+  return `The artist at index ${num} is ${artistsArray[num].name}`
 }  
+
+getArtistByIndex(artists, 0)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called get20s() 
@@ -251,9 +261,11 @@ Create a function called `removeArtist` that takes two arguments:
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset and log the number 19.  
 */
-function removeArtist(/*Your Code Here*/) {
-   /*Your Code Here*/
+function removeArtist(array,num) {
+  array.splice(num,1)
+  return array
 }
+removeArtist(artists,0)
    
 
 /**
@@ -271,11 +283,20 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+const newArray = [
+  {id: 20,
+name: "Nathan Chagar", 
+years: "1999-2020",
+genre: "Web Design", 
+nationality: "American",
+bio: "Lorem ipsum dolor sit amet"
+}
+]
+function addArtist(array){
+    artists.push(array)
+    return artists
   }
-
-  
+addArtist(newArray)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Create a function called lotsOfArt() that takes one argument: 
