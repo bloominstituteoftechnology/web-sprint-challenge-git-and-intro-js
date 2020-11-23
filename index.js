106c2d31a1a5ddcf0ee90853e272da153721f5f9
nicole pizzano <pizzano.nicole@gmail.com>
2020-11-22 22:07:29 -0500

    saving changes

--- index.js
+++ index.js
@@ -218,6 +218,7 @@ console.log(artists[2]);
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 artists.splice(9, 1, "Vincent Van Gogh");
+console.log(artists[9]);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -248,7 +249,7 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
+function get20s(artists){
   /*Your Code Here*/
 }
 
