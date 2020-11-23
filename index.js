f4d6896d99569604bf82a211e9b64851ee4aa053
victoriatrac <victoriatrac@gmail.com>
2020-11-22 17:57:24 -0700

    completed tasks 2a and 2b

--- index.js
+++ index.js
@@ -356,11 +356,15 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/* Code here */){
+// function getHTML(data){
 
-    /* Code here */
+//     for (const property in data) {
+//       const HTML = 
+//     }
 
-  }
+// }
+
+// console.log(getHTML(artists));
 
 
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
