c5a76b430607074e978eacd92fa06b3a09d9c93c
victoriatrac <victoriatrac@gmail.com>
2020-11-22 18:27:45 -0700

    attempted js stretch, lol

--- index.js
+++ index.js
@@ -356,16 +356,9 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-// function getHTML(data){
+function getHTML(data){
 
-//     for (const property in data) {
-//       const HTML = 
-//     }
-
-// }
-
-// console.log(getHTML(artists));
-
+}
 
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
