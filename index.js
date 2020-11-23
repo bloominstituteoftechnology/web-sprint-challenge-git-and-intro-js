45dee322d6893b593eb49e2a49c5bbb82b1a4264
Pape Ndiaye <pape@Papes-MacBook-Pro.local>
2020-11-22 23:36:59 -0500

    Task 6 complete

--- index.js
+++ index.js
@@ -283,9 +283,18 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+function addArtist(object){
+  object.push({ 
+      id: 20,
+      name: "Pape Ndiaye", 
+      years: 1995 - "current day",
+      genre: "Web Design", 
+      nationality: "African-American",
+      bio: "Talented individual, hard worker",
+    }  );
+}
+
+addArtist(artists);
 
   
 
