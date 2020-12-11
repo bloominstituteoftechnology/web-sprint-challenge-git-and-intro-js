afaceb5be12be4ca2ea9ce2f933ce93e406e4eca
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2020-12-11 18:39:58 -0500

    Task 3 completed

--- index.js
+++ index.js
@@ -230,8 +230,8 @@ console.log(artists[9].name)
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(taskThreeArray, desiredIndex) {
+  return `the artist at index ${desiredIndex} is ${taskThreeArray[desiredIndex].name}`
 }  
 
 
