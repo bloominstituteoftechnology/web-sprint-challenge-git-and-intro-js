b78666255f3a2d72dd8e6f02815db2ea80b7d8cb
Ryan Ishimaru <rishimaru23@gmail.com>
2021-01-22 10:59:57 -0800

    finished js

--- index.js
+++ index.js
@@ -244,8 +244,17 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(array, year){
+function get20s (array){
   const deathByYear=[];
+    for(let i = 0; i< array.length; i++){
+    if(array[i].years.split(" - ")[0] >= 1900 && array[i].years.split(" - ")[1] <= 2000)
+    deathByYear.push(array[i].name);
+}
+return deathByYear
+}
+console.log(get20s(artists));
+
+
 
 
 
