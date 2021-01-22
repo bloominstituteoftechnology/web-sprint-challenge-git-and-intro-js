6b93fe922b3d23bfa8cd0c36cb5f41885c4723e5
LaraHilborn <larahilborn@gmail.com>
2021-01-22 11:12:30 -0500

    completed task

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
 function get20s(/*Your Code Here*/){
-  if ()
+  
   //unsure how to do this when the years are in the object as strings//
 }
 
