0b36cc84a41dfc79be9b4cb77eb5e1e77ae27d75
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2021-02-07 18:04:23 -0500

    codegrade

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ function get20s(artists){
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
  function removeArtist(artists){
-  return(artists.length)
+  return(artists.length);
 }
 
 console.log(removeArtist(artists.splice(6,1)))
