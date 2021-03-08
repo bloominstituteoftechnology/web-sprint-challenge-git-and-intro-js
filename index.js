b6f43f077c70fc892387ae22fe87204697eb3ae0
timothydennis1994 <78184844+timothydennis1994@users.noreply.github.com>
2021-03-07 23:55:16 -0700

    Sprint 2

--- index.js
+++ index.js
@@ -209,15 +209,18 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
+console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log(artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name ='Vincent van Gogh';
+console.log(artists[8]);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,8 +231,9 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(numbers) {
+  const numbers = artists[i].id
+  return 'the artist at index ${this.id} is ${this.name}'
 }  
 
 
@@ -242,9 +246,10 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(lifespans){
+  if (artists.years[0] >= "1900" && artists.years[1] <= "2000")return artists.names
 }
+
 
 
 
@@ -278,8 +283,19 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(id, name, years, genre, nationality, bio){
+    const newArtist = {idN: id, nameN: name, yearsN: years, genreN: genre, nationalityN: nationality, bioN: bio}
+    newArtist.id = 20,
+    newArtist.name = 'Timothy Dennis',
+    newArtist.years = '1994 - Present',
+    newArtist.genre = 'Web Development',
+    newArtist.nationality = 'American',
+    newArtist.bio = "Attempting this but am struggling fiercely with JavaScript",
+    return newArtist
+}
+console.log(addArtist);
+artists.push (artists)
+    
   }
 
   
@@ -291,9 +307,11 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(){
+  if (artists.paintings > 100)
+  return artists.names
 }
+console.log(lotsOfArt)
 
 
 
