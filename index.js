c28fc59313b21b08ae3041d2c13e2ac0b2a04219
Anwar <AnwarAldogmi@gmail.com>
2021-03-19 11:04:31 -0700

    finished task 3

--- index.js
+++ index.js
@@ -229,10 +229,11 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
-}  
+function getArtistByIndex(array, number) {
+  return (`the artist at index ${number} is ${array[number].name}`)
+}
 
+console.log(getArtistByIndex(artists, 0));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
