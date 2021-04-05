fcbbbbf64210d15979683b7417e978c035fbf5b5
scoutreilly <scouty33@gmail.com>
2021-04-04 22:11:31 -0600

    task 3, 4 & 5

--- index.js
+++ index.js
@@ -229,10 +229,11 @@ console.log(artists[8].name);
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(list, index) {
+  var selectedIndex = list[index];
+  return `the artist at index ${index} is ${selectedIndex.name}`
 }  
-
+getArtistByIndex(artists, 0);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -243,10 +244,19 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(list){
+  let my20sBabies = [];
+  for (let i = 0; i <list.length; i++) {
+    let years = list[i].years.split('-');
+    let born = Number(years[0]);
+    let died = Number(years[1]);
+    if (born >= 1900 && died <= 2000) {
+      my20sBabies.push(list[i].name);
+    }
+  }
+  return my20sBabies
 }
-
+get20s(artists);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -258,10 +268,11 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(list, index){
+   list.splice(index, 1);
+   return list.length;
 }
-   
+removeArtist(artists, 0);   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
