3484cf1543982b5b4a08f669fb361ee7b78672b9
IfeIge <79335183+IfeIge@users.noreply.github.com>
2021-04-16 12:59:48 -0400

    task 1 and 2 complete

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ export const artists = [
       "wikipedia": "http://en.wikipedia.org/wiki/Vincent_van_Gogh",
       "paintings": 877
     },
+    
     {
       "id": 9,
       "name": "Gustav Klimt",
@@ -217,6 +218,7 @@ console.log(artists[2].bio)
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name = "Vincent Van Gogh"
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
