c210a3848fee0bfd277d7d942d850e820f4b6374
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-04-30 21:04:16 -0400

    Second Commit - All Answered / 1 error

--- index.js
+++ index.js
@@ -264,8 +264,9 @@ function get20s(array){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist()){
-   /*Your Code Here*/
+function removeArtist(array, index){
+  array.shift(index)
+  return array.length
 }
    
 
