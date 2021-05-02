bf4da3b1f154d08ac05dfafb3b19ba4d93e112e1
Gabe Chilson <g.chilson34@gmail.com>
2021-05-02 12:22:44 -0600

    finished responsive CSS for 500px

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
+function get20s(arr){
   /*Your Code Here*/
 }
 
