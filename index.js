0d0b3bf3ed0b88ab9ceebdfe6dec58c24ffcb195
Constance Beebe <constancebeebe@gmail.com>
2021-05-02 21:52:40 -0400

    Questions 1-4 answered.

--- index.js
+++ index.js
@@ -209,15 +209,29 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
+//ANSWER:
+// const firstName = 'Amedeo';
+// const lastName = 'Modigliani'; 
+// const whoIs = `The artist at index 0 is ${firstName} ${lastName}`;
+// console.log(whoIs);
+
+//console.log(artists[0]);
+
 
 //(2) Bio of the third artist (2nd index) in the array 
 
+//ANSWER:
+//console.log(artists[2].bio);  
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
-There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
+There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. 
+Use an array method to fix this issue and console.log() to check your work. */
 
+//ANSWER:
+
+console.log(artists[8].name = "Vincent Van Gogh")
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,9 +242,9 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
-}  
+ function getArtistByIndex(array, num){
+    return `the artist at index ${array[num].id} is ${array[num].name}`;
+} 
 
 
 
@@ -242,8 +256,21 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  let newArray = []
+  for (let i=0; i < array.length; i++){
+    const artistsCentury = array[i].years.split(' ');
+    // const firstYear => artistsCentury[1900];
+    // const secondYear <= artistsCentury[2000];
+
+    let firstYear = artistsCentury[0];
+    let secondYear = artistsCentury[2];
+
+    if(firstYear >= 1900 && secondYear <= 2000){
+      newArray.push(array[i].name);
+    }
+  }
+  return newArray
 }
 
 
@@ -255,9 +282,10 @@ function get20s(/*Your Code Here*/){
  3. The function should remove an artist from the array at the index
  4. Return the length of the remaining dataset.
  
- For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
+ For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our 
+ aset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
+function removeArtist(array, num){
    /*Your Code Here*/
 }
    
