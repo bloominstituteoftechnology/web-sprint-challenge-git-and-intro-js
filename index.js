d8b8eb4d9c693dd21c04b934b92f49e3058e3dde
David Spear <davidspear@Davids-MacBook-Pro.local>
2021-05-03 01:40:07 -0400

    finished tasks 6 and 7, still need 4

--- index.js
+++ index.js
@@ -272,7 +272,7 @@ function get20s(array){
 
 function removeArtist(array, index){
    array.splice(index, 1);
-   return (array.length);
+   return array.length;
 
 }
 console.log(removeArtist(artists,0));
@@ -294,10 +294,20 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    array.push(
+      {
+      id: 20,
+      name: "David Spear", 
+      years: "1997 - 2021",
+      genre: "Web Design", 
+      nationality: "American",
+      bio: "lorem ipsum"
+      }
+    )
+    return array;
   }
-
+console.log(addArtist(artists));
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -307,8 +317,14 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  const paintOneHundred = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].paintings > 100){
+    paintOneHundred.push(array[i].name);
+    }
+  }
+  return paintOneHundred;
 }
 
 
