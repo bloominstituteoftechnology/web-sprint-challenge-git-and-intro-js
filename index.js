0aa1be531891b6c18e7d5d69945c426d78814c75
BrandonWorobi <79031043+BrandonWorobi@users.noreply.github.com>
2021-05-03 00:36:34 -0400

    Attempting to fix codegrade

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ Practice accessing data above by console.log-ing following items:
 //(1) Name of the first artist (0th index) in the array
 
 
-//(2) Bio of the third artist (2nd index) in the array 
+
 
 
 
