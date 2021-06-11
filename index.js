a2a3acef735bdf0accdf9cef013a70bf4245a752
Rick <rmansfield@realpeoplesratings.com>
2021-06-11 14:29:53 -0500

    Task 1 (1) complete

--- index.js
+++ index.js
@@ -208,6 +208,7 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
+console.log('Practice 1:', artists[0]['name']);
 
 
 //(2) Bio of the third artist (2nd index) in the array 
