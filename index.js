21cc49b60576a1314a923541a743e90b225969db
Rick <rmansfield@realpeoplesratings.com>
2021-06-11 16:40:33 -0500

    Task 6 passed but somethings off

--- index.js
+++ index.js
@@ -287,9 +287,18 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    array.push({
+      id: "20",
+      name: "Your Name Here", 
+      years: "Your Birth Year - current day",
+      genre: "Web Design", 
+      nationality: "Your Nationality Here",
+      bio: "Add 1-2 sentences (or use lorem ipsum)"
+    });
+    return array;
   }
+  console.log('Task 6', artists[20]);
 
   
 
