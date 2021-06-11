d212c4c192e19d299ebc3e947cababe7099976e1
Rick <rmansfield@realpeoplesratings.com>
2021-06-11 15:57:28 -0500

    saving task 4 work and moving on

--- index.js
+++ index.js
@@ -244,10 +244,16 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  const newArray = [];
+  for (let i = 0; i < array.length; i++){
+    if(!array[i]["years"].includes("18")){
+      newArray.push(array[i]["name"]);
+    }
+  }
+  return newArray;
 }
-
+console.log('Task 4:', get20s(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -259,10 +265,11 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
-}
-   
+function removeArtist(array, id){
+  array.slice(id, 1);
+  return array.length;
+  }
+  console.log('task 5', removeArtist(artists, 0)); 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
