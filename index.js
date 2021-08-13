671280d6b9647ad0e901962052aacb85e9a48cae
peterdavidconley <85258866+peterdavidconley@users.noreply.github.com>
2021-08-13 08:02:29 -0400

    completed task 6

--- index.js
+++ index.js
@@ -284,8 +284,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  const newObject = {
+    id: 20,
+    name: "Your Name Here",
+    years: "Your Birth Year - current day",
+    genre: "Web Design",
+    nationality: "Your Nationality Here",
+    bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  }
+  array.push(newObject)
+  return array
 }
 
 
