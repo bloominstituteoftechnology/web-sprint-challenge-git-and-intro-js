1012d503b2cb5c2962139d6484dde78bfed674af
Bennett Kautz <bennettkautz@gmail.com>
2022-01-14 15:38:35 -0500

    finsished task 9

--- index.js
+++ index.js
@@ -335,9 +335,17 @@ Use artistByCountry to do the following:
 For example artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array, nationality){
+  const newArray = [];
+  for (let i = 0; i < array.length; i++){
+    if(array[i]["nationality"] === nationality){
+      newArray.push(array[i]["name"]);
+    }
+  }
+  return newArray;
 }
+
+console.log(artistByCountry(artists, 'Spanish'));
 
 
 
