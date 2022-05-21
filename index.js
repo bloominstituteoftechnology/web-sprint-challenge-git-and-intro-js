411c87e7f5630fe3bc4dc70beca1ac3e07307715
tkbchv <gedeon.tkebuchava@gmail.com>
2022-05-21 11:29:48 -0400

    task updated

--- index.js
+++ index.js
@@ -248,8 +248,12 @@ Use copy to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(/*Your Code Here*/) {
-  /*Your Code Here*/
+function listOfNames(artists) {
+  const retList = [];
+  artists.forEach(artist => {
+      retList[artist.id] = artist.name;
+  });
+  return retList;
 }
 
 
@@ -263,9 +267,10 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function removeArtist(artists, idx) {
+  if (!artists[idx]) {
+    throw new Error('invalid index');
+  }
 
 
 
@@ -284,9 +289,15 @@ Use addArtist to do the following:
 4. Return the array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function addArtist(artists, name, years, genre, nationality, bio) {
+  artists.push({
+      id: artists.length + 1,
+      name,
+      years,
+      genre,
+      nationality,
+      bio,
+  });
 
 
 
@@ -298,8 +309,8 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(artists) {
+  return artists.filter(artist => artist.paintings && artist.paintings > 100);
 }
 
 
@@ -314,8 +325,12 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(artists, name) {
+  const filteredArtists = artists.filter(artist => artist.name && artist.name === name);
+  if (filteredArtists.length > 0){
+      return filteredArtists[0].bio;
+  } 
+  return `artist info not found ${name}`;
 }
 
 
@@ -329,8 +344,13 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(artists, nationality){
+  const filteredArtists = artists.filter(artist => artist.nationality && artist.nationality.indexOf(nationality) !== -1);
+  
+  if (filteredArtists.length > 0) {
+      return filteredArtists.map(artist => artist.name);
+  }
+  return `No artists found for nationality: ${nationality}`;
 }
 
 
