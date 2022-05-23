556f7b354208d1de182e676ef26bb6b9a3fa965b
tkbchv <gedeon.tkebuchava@gmail.com>
2022-05-23 12:49:21 -0400

    solved

--- index.js
+++ index.js
@@ -233,13 +233,10 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(artists, idx) {
-  if (!artists[idx]) {
-    return 'invalid index, name cannot be determined';
-  }
-  
-  const { id, name } = artists[idx];
-  return `the artist at index ${id} is ${name}`;
+function getArtistByIndex(artists,idx){
+if(!artists[idx]){
+  return'invalid index, name cannot be determined';
+}const { id, name } = artists[idx];return `the artist at index ${id} is ${name}`;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -250,10 +247,10 @@ Use copy to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(artists) {
+function listOfNames(artists){
   const retList = [];
   artists.forEach(artist => {
-      retList[artist.id] = artist.name;
+    retList[artist.id] = artist.name;
   });
   return retList;
 }
@@ -269,10 +266,10 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function listOfNames(artists) {
+function listOfNames(artists){
   const retList = [];
   artists.forEach(artist => {
-      retList[artist.id] = artist.name;
+    retList[artist.id] = artist.name;
   });
   return retList;
 }
@@ -294,16 +291,16 @@ Use addArtist to do the following:
 4. Return the array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(artists, name, years, genre, nationality, bio) {
-	artists.push({
-		id: artists.length + 1,
-		name,
-		years,
-		genre,
-		nationality,
-		bio,
-	});
-	return artists;
+function addArtist(artists, name, years, genre, nationality, bio){
+  artists.push({
+    id: artists.length + 1,
+    name,
+    years,
+    genre,
+    nationality,
+    bio,
+  });
+  return artists;
 }
 addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development',
 'African American', 'I have a background in customer service at Big Retail Chain',
@@ -319,7 +316,7 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(artists) {
+function lotsOfArt(artists){
   return artists.filter(artist => artist.paintings && artist.paintings > 100);
 }
 
@@ -335,11 +332,11 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(artists, name) {
+function artistInfo(artists, name){
   const filteredArtists = artists.filter(artist => artist.name && artist.name === name);
   if (filteredArtists.length > 0){
-      return filteredArtists[0].bio;
-  } 
+    return filteredArtists[0].bio;
+  }
   return `artist info not found ${name}`;
 }
 artistInfo(artists, 'Frida Kahlo')
@@ -357,7 +354,7 @@ function artistByCountry(artists, nationality){
   const filteredArtists = artists.filter(artist => artist.nationality && artist.nationality.indexOf(nationality) !== -1);
   
   if (filteredArtists.length > 0) {
-      return filteredArtists.map(artist => artist.name);
+    return filteredArtists.map(artist => artist.name);
   }
   return `No artists found for nationality: ${nationality}`;
 }
