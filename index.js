5053daaaa698c07942ae04d2c038294aab9f478c
Christopher-Martinez-422 <107010274+Christopher-Martinez-422@users.noreply.github.com>
2022-06-11 21:38:41 -0700

    Completed Sprint Challenge

--- index.js
+++ index.js
@@ -209,17 +209,18 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-
+console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log(artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 (not auto tested): 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Fix this issue and console.log() to check your work. */
 
-
+const vincentVanDough = artists[8].name = 'Vincent Van Gogh'
+console.log(vincentVanDough)
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
 Use getArtistByIndex to do the following:
@@ -230,10 +231,10 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artists, id) { //define parameters
+  return `the artist at index ${id} is ${artists[id].name}`
 }
-
+console.log(getArtistByIndex(artists,0));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -244,10 +245,16 @@ Use copy to do the following:
 🌟 EXAMPLE of return: ["Amedeo Modigliani", "Vasiliy Kandinskiy", "Diego Rivera"....]
 */
 
-function listOfNames(/*Your Code Here*/) {
-  /*Your Code Here*/
+function listOfNames(artists) {
+  let artistsNames = [] //new array
+  for (let i = 0; i < artists.length; i++){ //loop
+    let name = artists[i].name //define parameters
+    artistsNames.push(name); //Salt-N-Pepa
+  }   
+  return artistsNames;
 }
 
+console.log(listOfNames(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -259,11 +266,17 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(artists, removedIndex) { //define parameters
+  let newList = []; //new array
+  for (let i = 0; i < artists.length; i++) { //loop
+    if (i != removedIndex) { //define parameters
+      newList.push(artists[i]); //Salt-N-Pepa
+    }
+  }
+  return newList;
 }
 
-
+console.log(removeArtist(artists, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -280,9 +293,12 @@ Use addArtist to do the following:
 4. Return the array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(artists, name, years, genre, nationality, bio){ //define parameters
+  let object = {name: name, years: years, genre: genre, nationality: nationality, bio: bio} //new object requiring input
+  artists.push(object); //Salt-N-Pepa
+      return artists;
 }
+console.log(addArtist(artists, 'Christopher Martinez', '1992 - 2022', 'Web Design', 'Colombian', 'lorem ipsum')); //object input
 
 
 
@@ -294,9 +310,18 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(artists){ //define parameters
+  let result = [] //new array
+  for(let i = 0; i < artists.length; i++){ //loop
+    let paintings = artists[i].paintings //new key
+    if(paintings > 100){ //parameter
+      result.push(artists[i].name) //Salt-N-Pepa
+  }
 }
+
+return result;
+}
+console.log(lotsOfArt(artists));
 
 
 
@@ -310,9 +335,15 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(artists,name){ //define parameters
+  for (let i = 0; i < artists.length; i++) { //loop
+    if (artists[i].name == name) { //define parameters
+      return artists[i].bio
+    }
+  }
 }
+
+console.log(artistInfo(artists, 'Frida Kahlo')) //required input
 
 
 
@@ -326,9 +357,17 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(artists,nationality){ //define parameters
+  let nationsArtists = [] //new array
+  for (let i = 0; i < artists.length; i++) { //loop
+    if (artists[i].nationality == nationality) { //define parameters
+      nationsArtists.push(artists[i].name) //Salt-N-Pepa
+    }
+  }
+  return nationsArtists
 }
+
+console.log(artistByCountry(artists, 'Spanish')) //required input
 
 
 
